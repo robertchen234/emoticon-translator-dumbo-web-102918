3f6d85ce76184a1006ecbd2f88d5ba02383623a5
Robert Chen <robertchen234@gmail.com>
2018-10-25 17:03:44 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,8 @@ def load_library(path)
   library = YAML.load_file(path)
   lib_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
-    # the keys 'get_meaning' and 'get_emoticon' point to inner hashes
+    lib_hash["get_meaning"][emoticons[1]] = meaning
+    lib_hash["get_emoticon"][emoticons[0]] = emoticons[1]
     # the keys inside the 'get_meaning' hash are the Japanese emoticons
     # the emoticon keys inside the 'get_meaning' hash point to their meanings
     # the keys inside the 'get_emoticon' hash are the English emoticons

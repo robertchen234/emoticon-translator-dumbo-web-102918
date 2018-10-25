61dfa4c4581848f3579fce46acac787f8331541b
Robert Chen <robertchen234@gmail.com>
2018-10-25 16:50:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path)
   library = YAML.load_file(path)
   lib_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
-    
+    lib_hash["get_meaning"] = 
   end
   lib_hash
 end
